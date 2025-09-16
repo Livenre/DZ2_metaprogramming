4f68d14978b9c58dc5b5b0707f07b3c196ccb73d
Livenre <livenre@gmail.com>
2025-09-17 02:22:18 +0300

    Add project files.

---
+++ DZ2/Counter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ2
+{
+    public sealed class Counter
+    {
+        private int _value;
+
+        public int Value
+        {
+            get { return this._value; }
+        }
+
+        public Counter() : this(0) { }
+
+        public Counter(int start)
+        {
+            if (start < 0)
+            {
+                throw new ArgumentException("Початкове значення не може бути від’ємним", nameof(start));
+            }
+            this._value = start;
+        }
+
+        public void Increment()
+        {
+            this._value++;
+        }
+
+        public void Decrement()
+        {
+            if (this._value == 0)
+            {
+                throw new Exception("Значення не може бути від’ємним");
+            }
+            this._value--;
+        }
+
+        public bool TryDecrement()
+        {
+            if (this._value == 0)
+            {
+                return false;
+            }
+            this._value--;
+            return true;
+        }
+
+        public void Reset()
+        {
+            this._value = 0;
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ DZ2/TodoItem.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ2
+{
+    public sealed class TodoItem
+    {
+        private string _title;
+        private bool _isDone;
+
+        public string Title
+        {
+            get { return this._title; }
+        }
+
+        public bool IsDone
+        {
+            get { return this._isDone; }
+        }
+
+        public TodoItem(string title) : this(title, false) { }
+
+        public TodoItem(string title, bool isDone)
+        {
+            if (string.IsNullOrWhiteSpace(title))
+            {
+                throw new ArgumentException("Задача пуста або написана пробілами", nameof(title));
+            }
+            this._title = title;
+            this._isDone = isDone;
+        }
+
+        public void MarkDone()
+        {
+            this._isDone = true;
+        }
+
+        public void MarkUndone()
+        {
+            this._isDone = false;
+        }
+
+        public bool TryRename(string newTitle)
+        {
+            if (string.IsNullOrWhiteSpace(newTitle))
+            {
+                return false;
+            }
+            this._title = newTitle;
+            return true;
+        }
+    }
+}
+
+
+
+
+
+
+
+
